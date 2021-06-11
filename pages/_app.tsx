8e3bce66451bfa5fc0bcc3a2279575f754a88a98
Giacomo Forlani <giacomo.forlani@enhancers.it>
2021-06-12 01:10:53 +0200

    fix: register gsap plugin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
+import { gsap, CSSPlugin } from 'gsap';
 
 import '../styles/globals.scss';
+
+gsap.registerPlugin(CSSPlugin);
 
 const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
