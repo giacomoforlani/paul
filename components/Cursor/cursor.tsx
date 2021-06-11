4e4bdd6302cbdc9092dede0e82e9f6a638d67782
Giacomo Forlani <giacomo.forlani@enhancers.it>
2021-06-12 00:56:22 +0200

    feat: cursor

--- components/Cursor/cursor.tsx
+++ components/Cursor/cursor.tsx
@@ -1,33 +1,73 @@
-import React, { useEffect, useState } from 'react';
+import { TweenLite, Linear } from 'gsap';
+import React, {
+  useCallback, useEffect, useRef, useState,
+} from 'react';
 
 import styles from './cursor.module.scss';
 
 const Cursor = () => {
-  const [outPos, setOutPos] = useState({ top: 0, left: 0 });
-  const [inPos, setInPos] = useState({ top: 0, left: 0 });
+  const bigCursorRef = useRef(null);
+  const smallCursorRef = useRef(null);
+
+  const [isMouseDown, setIsMouseDown] = useState(false);
+
+  const onMouseMove = useCallback(
+    (e: MouseEvent) => {
+      if (bigCursorRef.current) {
+        TweenLite.to(bigCursorRef.current, 0.4, {
+          x: e.clientX,
+          y: e.clientY,
+          force3D: true,
+          rotation: 0.01,
+        });
+      }
+
+      if (smallCursorRef.current) {
+        TweenLite.to(smallCursorRef.current, 0, {
+          x: e.clientX,
+          y: e.clientY,
+          force3D: true,
+          rotation: 0.01,
+          ease: Linear.easeNone,
+        });
+      }
+    },
+    [],
+  );
+
+  const onMouseDown = useCallback(() => {
+    setIsMouseDown(true);
+  }, []);
+
+  const onMouseUp = useCallback(() => {
+    setTimeout(() => setIsMouseDown(false), 300);
+  }, []);
 
   useEffect(() => {
-    const listener = (event: MouseEvent) => {
-      const { clientY: top, clientX: left } = event;
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mousedown', onMouseDown);
+    document.addEventListener('mouseup', onMouseUp);
 
-      setOutPos({ top, left });
-      setTimeout(() => setInPos({ top, left }), 100);
+    return () => {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mousedown', onMouseMove);
+      document.removeEventListener('mouseup', onMouseMove);
     };
-
-    document.addEventListener('mousemove', listener);
-
-    return () => document.removeEventListener('mousemove', listener);
-  }, []);
+  }, [onMouseMove, onMouseDown, onMouseUp]);
 
   return (
     <>
       <div
-        className={styles.Cursor__Out}
-        style={{ ...outPos }}
+        className={[
+          styles.Cursor__Big,
+          isMouseDown ? styles['Cursor__Big--Full'] : '',
+        ].join(' ')}
+        ref={bigCursorRef}
       />
+
       <div
-        className={styles.Cursor__In}
-        style={{ ...inPos }}
+        className={styles.Cursor__Small}
+        ref={smallCursorRef}
       />
     </>
   );
--- components/Noise/noise.tsx
+++ components/Noise/noise.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 import styles from './noise.module.scss';
 
 const Noise = () => (
-  <div className={styles.Noise} />
+  <div className={styles.Noise}>
+    <div className={styles.Noise__Background} />
+  </div>
 );
 
 export { Noise };
